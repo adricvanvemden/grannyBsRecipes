88628ef91c4cee9292e12c6603caa3135aecf7e3
Sarah Mcmahon <adricvanvemden@gmail.com>
2024-02-18 19:19:29 +0100

    add shadcn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,9 @@
+import { Button } from '@/components/ui/button';
 
 export default function Home() {
   return (
     <main className="">
-      Page
+      <Button>Click me</Button>
     </main>
   );
 }
